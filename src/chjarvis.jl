b77259a88d3600993ca2702bf00dce03bfd3f02e
Carbon225 <carbon225@protonmail.com>
2021-11-07 19:32:21 +0100

    COLL

--- src/chjarvis.jl
+++ src/chjarvis.jl
@@ -21,11 +21,23 @@ function getfirstpoint(pnts)
 end
 
 function getnextpoint(pnts, i0)
+    A = pnts[i0]
     i1 = i0 == 1 ? 2 : 1
 
     for i in filter(i -> i != i0, 1:length(pnts))
-        if orient(pnts[i0], pnts[i1], pnts[i]) < 0
+        B = pnts[i1]
+        C = pnts[i]
+
+        o = orient(A, B, C) 
+
+        if o < 0
             i1 = i
+        elseif o == 0
+            normAB = sqrt((A.x - B.x)^2 + (A.y - B.y)^2)
+            normAC = sqrt((A.x - C.x)^2 + (A.y - C.y)^2)
+            if normAC > normAB
+                i1 = i
+            end
         end
     end
 
@@ -34,16 +46,16 @@ end
 
 function chjarvis(pnts::AbstractVector{Point{T}})::AbstractVector{Point{T}} where T
     i0 = getfirstpoint(pnts)
-    i1 = getnextpoint(pnts, i0)
+    i = getnextpoint(pnts, i0)
 
     ch::Vector{Point{T}} = [
         pnts[i0],
-        pnts[i1],
+        pnts[i],
     ]
 
-    while i1 != i0
-        i1 = getnextpoint(pnts, i1)
-        push!(ch, pnts[i1])
+    while i != i0
+        i = getnextpoint(pnts, i)
+        push!(ch, pnts[i])
     end
 
     ch

6048ca2208a5bb2a0b7eb8f17df00c2ca04fe156
Carbon225 <carbon225@protonmail.com>
2021-11-07 21:06:02 +0100

    mergesort

--- src/chgraham.jl
+++ src/chgraham.jl
@@ -21,13 +21,85 @@ function ltorient(pnts, ia) where T
     end
 end
 
-function sortbyangle(pnts, i0)
-    sort(1:length(pnts), lt=ltorient(pnts, i0))
+function sortbyangle(pnts::AbstractVector{Point{T}}, i0::Integer)::AbstractVector{Integer} where T
+    lt = ltorient(pnts, i0)
+
+    function merge(t1, t2)
+        t = empty(t1)
+        
+        i1 = 1
+        n1 = length(t1)
+        
+        i2 = 1
+        n2 = length(t2)
+
+        while i1 <= n1 || i2 <= n2
+
+            # if one array empty
+
+            if i2 > n2
+                push!(t, t1[i1])
+                i1 += 1
+                continue
+            end
+
+            if i1 > n1
+                push!(t, t2[i2])
+                i2 += 1
+                continue
+            end
+
+            # if one element < other
+
+            if lt(t1[i1], t2[i2])
+                push!(t, t1[i1])
+                i1 += 1
+                continue
+            end
+
+            if lt(t2[i2], t1[i1])
+                push!(t, t2[i2])
+                i2 +=1
+                continue
+            end
+
+            # equal orient
+
+            normAB = abs(pnts[i0] - pnts[t1[i1]])
+            normAC = abs(pnts[i0] - pnts[t2[i2]])
+
+            if normAB > normAC
+                push!(t, t1[i1])
+            else
+                push!(t, t2[i2])
+            end
+            i1 += 1
+            i2 += 1
+        end
+
+        t
+    end
+
+    function sort(t)
+        n = length(t)
+        if n < 2
+            return t
+        end
+
+        imid = (1 + length(t)) ÷ 2
+        left = t[1:imid]
+        right = t[imid+1:end]
+        merge(sort(left), sort(right))
+    end
+
+    sort(1:length(pnts))
 end
 
 function chgraham(pnts::AbstractVector{Point{T}})::AbstractVector{Point{T}} where T
     i0 = getbottomleftpoint(pnts)
     inds = sortbyangle(pnts, i0)
+    @assert i0 == inds[1]
+    i1 = inds[2]
     pnts[inds]
 end
 
